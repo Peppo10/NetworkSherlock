00fdbf675d6a341c5945793ee492ddcec7df7c8a
giuseppe liace <giuseppeliace10@gmail.com>
2024-10-19 17:17:08 +0200

    Refactor CVE_Scanner_Main

--- plugins/main.py
+++ plugins/main.py
@@ -20,31 +20,32 @@ class CVE_Scanner_Main:
         http_url = f"http://{self.ip}"
         https_url = f"https://{self.ip}"
         try:
-            if '443' in ports:
-                heartbleed_scanner = HeartbleedScanner(https_url)
-                results['Heartbleed'] = heartbleed_scanner.check_vulnerability()
+            if ['443', '80'] in ports:
+                shellshock_scanner = ShellshockScanner(f"{http_url}/cgi-bin/test.cgi")
+                results['Shellshock'] = shellshock_scanner.scan_for_shellshock()
 
-            for port in ['80', '443']:
-                url = https_url if port == '443' else http_url
+                struts_scanner = ApacheStrutsScanner(f"{http_url}/showcase.action")
+                results['ApacheStruts'] = struts_scanner.scan_for_apache_struts()
 
-                if port in ports:
-                    shellshock_scanner = ShellshockScanner(f"{url}/cgi-bin/test.cgi")
-                    results['Shellshock'] = shellshock_scanner.scan_for_shellshock()
+                struts2_scanner = StrutsScanner(http_url)
+                results['CVE2018-11776'] = struts2_scanner.check_vulnerability()
 
-                    struts_scanner = ApacheStrutsScanner(f"{url}/showcase.action")
-                    results['ApacheStruts'] = struts_scanner.scan_for_apache_struts()
+                if ['443'] in ports:
+                    heartbleed_scanner = HeartbleedScanner(https_url)
+                    results['Heartbleed'] = heartbleed_scanner.check_vulnerability()
 
+                    cve20221388_scanner = CVE20221388Scanner(self.ip)
+                    results['CVE-2022-1388'] = cve20221388_scanner.check_vulnerability()
 
-                    struts2_scanner = StrutsScanner(url)
-                    results['CVE2018-11776'] = struts2_scanner.check_vulnerability()
-
-                    if port == '443':
-                        cve20200688_scanner = CVE20200688Tester(url)
-                        results['CVE-2020-0688'] = cve20200688_scanner.check_vulnerability()
+                    cve20200688_scanner = CVE20200688Tester(https_url)
+                    results['CVE-2020-0688'] = cve20200688_scanner.check_vulnerability()
 
             if '445' in ports:
                 smbghost_scanner = SMBVulnerabilityChecker(self.ip)
                 results['SMBGhost'] = smbghost_scanner.check_vulnerability()
+
+                cve202222784_scanner = CVE202222784Scanner(self.ip)
+                results['CVE-2022-22784'] = cve202222784_scanner.check_vulnerability()
 
             if '8009' in ports:
                 ghostcat_scanner = GhostcatScanner(self.ip)
@@ -57,14 +58,6 @@ class CVE_Scanner_Main:
             if '139' in ports or '445' in ports:
                 eternalblue_scanner = EternalBlueScanner(self.ip)
                 results['EternalBlue'] = eternalblue_scanner.scan_for_eternalblue()
-
-            if '445' in ports:
-                cve202222784_scanner = CVE202222784Scanner(self.ip)
-                results['CVE-2022-22784'] = cve202222784_scanner.check_vulnerability()
-
-            if '443' in ports:
-                cve20221388_scanner = CVE20221388Scanner(self.ip)
-                results['CVE-2022-1388'] = cve20221388_scanner.check_vulnerability()
         except:
             pass
         return results
